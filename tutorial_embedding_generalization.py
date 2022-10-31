f4a5148bd8ff4051c0141a61b4e527d8bf79021d
Rlag1998 <99010554+Rlag1998@users.noreply.github.com>
2022-10-31 15:48:50 +0000

    Update tutorial_embedding_generalization.py

--- tutorial_embedding_generalization.py
+++ tutorial_embedding_generalization.py
@@ -29,7 +29,7 @@ Illustrated below is the general circuit used.
 
 |
 
-.. figure:: ../demonstrations/embedding_metric_learning/classification.png
+.. figure:: ../embedding_metric_learning/classification.png
    :align: center
    :width: 90%
    
@@ -341,7 +341,7 @@ plt.colorbar(im, cax=cax)
 #
 # |
 #
-# .. figure:: ../demonstrations/embedding_metric_learning/figures/All_Figures/3.4.1.png
+# .. figure:: ../embedding_metric_learning/figures/All_Figures/3.4.1.png
 #   :align: center
 #   :width: 90%
 #
@@ -366,7 +366,7 @@ plt.colorbar(im, cax=cax)
 #
 # |
 #
-# .. figure:: ../demonstrations/embedding_metric_learning/figures/All_Figures/3.4.2.png
+# .. figure:: ../embedding_metric_learning/figures/All_Figures/3.4.2.png
 #   :align: center
 #   :width: 90%
 #
@@ -411,7 +411,7 @@ plt.legend(handles=[blue_patch, cornflowerblue_patch, red_patch, lightcoral_patc
 #
 # |
 #
-# .. figure:: ../demonstrations/embedding_metric_learning/figures/All_Figures/3.3.3.png
+# .. figure:: ../embedding_metric_learning/figures/All_Figures/3.3.3.png
 #   :align: center
 #   :width: 90%
 #
@@ -447,7 +447,7 @@ plt.legend(handles=[blue_patch, cornflowerblue_patch, red_patch, lightcoral_patc
 #
 # |
 #
-# .. figure:: ../demonstrations/embedding_metric_learning/figures/All_Figures/3.3.4.png
+# .. figure:: ../embedding_metric_learning/figures/All_Figures/3.3.4.png
 #   :align: center
 #   :width: 90%
 #
